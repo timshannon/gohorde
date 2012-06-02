ad6e65d91439f065c2f76df7230081c2467eef4f
Tim Shannon <tim@townsourced.com>
2012-06-02 12:23:06 -0500

    Last of the utility functions.  Next I'll build some quick examples / tests, then work on getting the 3-4 missing functions working properly.

--- horde3d/horde3dUtils.go
+++ horde3d/horde3dUtils.go
@@ -48,3 +48,36 @@ func H3dutCreateGeometryRes(name string, numVertices int, numTriangleIndices int
 		(*C.float)(unsafe.Pointer(&textData2[0]))))
 
 }
+
+//TODO: Implement
+//func H3dutCreateTGAImage(
+
+func H3dutScreenshot(filename string) bool {
+	cFilename := C.CString(filename)
+	defer C.free(unsafe.Pointer(cFilename))
+
+	return Bool[int(C.h3dutScreenshot(cFilename))]
+}
+func H3dutPickRay(cameraNode H3DNode, nwx float32, nwy float32, ox *float32, oy *float32, oz *float32,
+	dx *float32, dy *float32, dz *float32) {
+	C.h3dutPickRay(C.H3DNode(cameraNode), C.float(nwx), C.float(nwy), (*C.float)(unsafe.Pointer(ox)),
+		(*C.float)(unsafe.Pointer(oy)), (*C.float)(unsafe.Pointer(oz)),
+		(*C.float)(unsafe.Pointer(dx)), (*C.float)(unsafe.Pointer(dy)),
+		(*C.float)(unsafe.Pointer(dz)))
+}
+
+func H3dutPickNode(cameraNode H3DNode, nwx float32, nwy float32) H3DNode {
+	return H3DNode(C.h3dutPickNode(C.H3DNode(cameraNode), C.float(nwx), C.float(nwy)))
+}
+
+func H3dutShowText(text string, x float32, y float32, size float32, colR float32, colG float32, colB float32,
+	fontMaterialRes H3DRes) {
+	cText := C.CString(text)
+	defer C.free(unsafe.Pointer(cText))
+	C.h3dutShowText(cText, C.float(x), C.float(y), C.float(size), C.float(colR), C.float(colG),
+		C.float(colB), C.H3DRes(fontMaterialRes))
+}
+
+func H3dutShowFrameStats(fontMaterialRes H3DRes, panelMaterialRes H3DRes, mode int) {
+	C.h3dutShowFrameStats(C.H3DRes(fontMaterialRes), C.H3DRes(panelMaterialRes), C.int(mode))
+}
