5d0b0ce49765f5379fd2436400f4226e3a26226e
Tim Shannon <tim@townsourced.com>
2012-06-02 14:30:06 -0500

    Got basic sample to build, and it runs, but I need some actual content to test with.  Unfortunately the online docs are way out of date.

--- examples/sdl/sdl.go
+++ examples/sdl/sdl.go
@@ -13,9 +13,9 @@ import (
 
 func main() {
 	var running bool = true
-	var cam horde3d.H3DNode
 	var width int = 800
 	var height int = 600
+	var t float32
 
 	if sdl.Init(sdl.INIT_VIDEO) != 0 {
 		panic(sdl.GetError())
@@ -31,12 +31,6 @@ func main() {
 	horde3d.Init()
 	fmt.Println("Version: ", horde3d.GetVersionString())
 
-	//Setup Camera Viewport
-	horde3d.SetNodeParamI(cam, horde3d.H3DCamera_ViewportXI, 0)
-	horde3d.SetNodeParamI(cam, horde3d.H3DCamera_ViewportYI, 0)
-	horde3d.SetNodeParamI(cam, horde3d.H3DCamera_ViewportWidthI, width)
-	horde3d.SetNodeParamI(cam, horde3d.H3DCamera_ViewportHeightI, height)
-
 	//pipeline
 	pipeRes := horde3d.AddResource(horde3d.H3DResTypes_Pipeline, "standard.pipeline.xml", 0)
 	modelRes := horde3d.AddResource(horde3d.H3DResTypes_SceneGraph, "character.scene.xml", 0)
@@ -44,14 +38,44 @@ func main() {
 
 	horde3d.LoadResourcesFromDisk("")
 
-	model := horde3d.AddNodes(horde3d.H3DRootNode, modelRes)
+	//add camera
+	cam := horde3d.AddCameraNode(horde3d.RootNode, "Camera", pipeRes)
+	//Setup Camera Viewport
+	horde3d.SetNodeParamI(cam, horde3d.H3DCamera_ViewportXI, 0)
+	horde3d.SetNodeParamI(cam, horde3d.H3DCamera_ViewportYI, 0)
+	horde3d.SetNodeParamI(cam, horde3d.H3DCamera_ViewportWidthI, width)
+	horde3d.SetNodeParamI(cam, horde3d.H3DCamera_ViewportHeightI, height)
+
+	//add model
+	model := horde3d.AddNodes(horde3d.RootNode, modelRes)
 	horde3d.SetupModelAnimStage(model, 0, animRes, 0, "", false)
+	//add light
+	light := horde3d.AddLightNode(horde3d.RootNode, "Light1", 0, "LIGHTING", "SHADOWMAP")
+	horde3d.SetNodeTransform(light, 0, 20, 0, 0, 0, 0, 1, 1, 1)
+	horde3d.SetNodeParamF(light, horde3d.H3DLight_RadiusF, 0, 50)
+	horde3d.SetNodeParamF(light, horde3d.H3DLight_FovF, 0, 90)
+	horde3d.SetNodeParamI(light, horde3d.H3DLight_ShadowMapCountI, 3)
+	horde3d.SetNodeParamF(light, horde3d.H3DLight_ShadowSplitLambdaF, 0, 0.9)
+	horde3d.SetNodeParamF(light, horde3d.H3DLight_ShadowMapBiasF, 0, 0.001)
+	horde3d.SetNodeParamF(light, horde3d.H3DLight_ColorF3, 0, 0.9)
+	horde3d.SetNodeParamF(light, horde3d.H3DLight_ColorF3, 1, 0.7)
+	horde3d.SetNodeParamF(light, horde3d.H3DLight_ColorF3, 2, 0.75)
+
 	for running {
+		t = 0
+		//increase anim time
+		t = t + 10.0*(1/60)
+		//process SDL events / input
 		switch event := sdl.PollEvent(); event.(type) {
 		case *sdl.QuitEvent:
 			running = false
 			break
 		}
+		horde3d.SetModelAnimParams(model, 0, t, 1.0)
+		horde3d.SetNodeTransform(model,
+			t*10, 0, 0,
+			0, 0, 0,
+			1, 1, 1)
 		horde3d.Render(cam)
 		sdl.GL_SwapBuffers()
 	}
