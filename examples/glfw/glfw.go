ed2bf66a3d8c44c252dfdf0e96da0f43838f949a
Tim Shannon <tim@townsourced.com>
2012-06-19 22:14:59 -0500

    Removed redundant H3D prefix on all enumerations, as I've seen other bindings for horde do.  It makes it cleaner.  Still trying to get a simply glfw sample to work.

---
+++ examples/glfw/glfw.go
@@ -0,0 +1,108 @@
+//Copyright (c) 2012 Tim Shannon
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
+
+package main
+
+import (
+	"code.google.com/p/gohorde/horde3d"
+	"fmt"
+	"github.com/jteeuwen/glfw"
+	"os"
+)
+
+const (
+	caption   = "GLFW Sample (Go Implementation)"
+	appWidth  = 800
+	appHeight = 600
+)
+
+func main() {
+	var running bool = true
+
+	if err := glfw.Init(); err != nil {
+		fmt.Fprintf(os.Stderr, "[e] %v\n", err)
+		return
+	}
+
+	//ensure glfw is cleaned up
+	defer glfw.Terminate()
+
+	if err := glfw.OpenWindow(appWidth, appHeight, 8, 8, 8, 8,
+		24, 8, glfw.Windowed); err != nil {
+		fmt.Fprintf(os.Stderr, "[e] %v\n", err)
+		return
+	}
+	defer glfw.CloseWindow()
+
+	horde3d.Init()
+
+	horde3d.SetOption(horde3d.Options_DebugViewMode, 1)
+	// Add resources
+	//pipeline
+	pipeRes := horde3d.AddResource(horde3d.ResTypes_Pipeline, "hdr.pipeline.xml", 0)
+
+	sphereRes := horde3d.AddResource(horde3d.ResTypes_SceneGraph, "sphere/sphere.scene.xml", 0)
+
+	//load resources paths separated by |
+	horde3d.LoadResourcesFromDisk("../content|" +
+		"../content/pipelines|" +
+		"../content/models|" +
+		"../content/materials|" +
+		"../content/shaders|" +
+		"../content/textures|" +
+		"../content/effects")
+
+	model := horde3d.AddNodes(horde3d.RootNode, sphereRes)
+
+	// Add light source
+	light := horde3d.AddLightNode(horde3d.RootNode, "Light1", 0, "LIGHTING", "SHADOWMAP")
+	horde3d.SetNodeTransform(light, 0, 20, 0, 0, 0, 0, 1, 1, 1)
+	horde3d.SetNodeParamF(light, horde3d.Light_RadiusF, 0, 50)
+
+	//add camera
+	cam := horde3d.AddCameraNode(horde3d.RootNode, "Camera", pipeRes)
+	//Setup Camera Viewport
+	horde3d.SetNodeParamI(cam, horde3d.Camera_ViewportXI, 0)
+	horde3d.SetNodeParamI(cam, horde3d.Camera_ViewportYI, 0)
+	horde3d.SetNodeParamI(cam, horde3d.Camera_ViewportWidthI, appWidth)
+	horde3d.SetNodeParamI(cam, horde3d.Camera_ViewportHeightI, appHeight)
+	horde3d.SetupCameraView(cam, 45, float32(appWidth)/float32(appHeight), 0.5, 2048)
+	horde3d.ResizePipelineBuffers(pipeRes, appWidth, appHeight)
+	//enable vertical sync if the card supports it
+	glfw.SetSwapInterval(1)
+
+	glfw.SetWindowTitle(caption)
+
+	var i float32 = 5
+	for running {
+
+		i += 1
+		horde3d.SetNodeTransform(model, 0, 0, i, 0, 0, 0, 1, 1, 1)
+
+		horde3d.Render(cam)
+		horde3d.FinalizeFrame()
+		glfw.SwapBuffers()
+		running = glfw.Key(glfw.KeyEsc) == 0 &&
+			glfw.WindowParam(glfw.Opened) == 1
+	}
+
+	horde3d.DumpMessages()
+	horde3d.Release()
+}
